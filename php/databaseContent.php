9fae47cd4152d20807ba1f3605cee61426bf2e5b
Johawx <johannes@neu-haus.info>
2022-06-02 14:40:32 +0200

    Adds some backend

---
+++ php/databaseContent.php
@@ -0,0 +1,81 @@
+<?php
+require_once(__DIR__ . './../services/databaseContentService.php');
+require_once(__DIR__ . './endpoint.php');
+
+class DatabaseContentEndpoint extends Endpoint
+{
+    private $databaseContentService;
+    public function __construct() {
+        parent::__construct();
+        $this->databaseContentService = new DatabaseContentService($this->database);
+    }
+
+    protected function get() {
+        $result = null;
+        $id = $this->getQueryParameter('id');
+        if ($id= null) {
+            $result = $this->databaseContentService->getById($id);
+            if ($result == null) {
+                // set 404 - Not Found
+                $this->notFound();
+                return;
+            }
+        } else {
+            $result = $this->databaseContentService->getAll();
+        }
+        $this->ok($result);
+    }
+
+    protected function post() {
+        $id = $this->body['id'];
+
+        // check if body already exists
+        if ($this->databaseContentService->isExisting($id)) {
+            $this->duplicatedId();
+            return;
+        }
+
+        // insert into db if not exists
+        $this->databaseContentService->add($this->body);
+        $result = $this->databaseContentService->getById($id);
+        $this->created($result);
+    }
+
+    protected function patch() {
+        // get and parse http hody
+        // check if entity in db --> else 404
+        // update entity in db with latest properties
+
+        $id = $this->body['id'];
+
+        // check if entity already exists in database
+        if (!$this->databaseContentService->isExisting($id)) {
+            // set 404 - Not Found
+            $this->notFound();
+            return;
+        }
+
+        $this->databaseContentService->update($this->body);
+        $result = $this->databaseContentService->getById($id);
+        $this->ok($result);
+    }
+
+    protected function delete() {
+        $id = $this->getQueryParameter('id', true);
+
+        // check if entity already exists in database
+        if (!$this->databaseContentService->isExisting($id)) {
+            // set 404 - Not Found
+            $this->notFound();
+            return;
+        }
+
+        $result = $this->databaseContentService->getById($id);
+        $this->databaseContentService->deleteById($id);
+        $this->ok($result);
+    }
+}
+
+$endpoint = new DatabaseContentEndpoint();
+$endpoint->handleRequest();
+?>
---
+++ php/endpoint.php
@@ -0,0 +1,116 @@
+<?php
+class Endpoint
+{
+    protected $database;
+    protected $body;
+    protected $params;
+
+    public function __construct() {
+        // initialize database
+        // set headers
+        $this->database = new PDO('mysql:host=127.0.0.1;dbname=php_rest_api_db', "root", "");
+        $this->setHeaders();
+    }
+
+    protected function setHeaders() {
+        header('Content-Type: application/json');
+        header('Access-Control-Allow-Origin: http://127.0.0.1:5501');
+    }
+
+    protected function get() {
+        $this->unsupportedHttpMethod();
+    }
+
+    protected function post() {
+        $this->unsupportedHttpMethod();
+    }
+
+    protected function delete() {
+        $this->unsupportedHttpMethod();
+    }
+
+    protected function patch() {
+        $this->unsupportedHttpMethod();
+    }
+
+    protected function options() {
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS');
+    }
+
+    public function handleRequest() {
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+
+        $bodyString = file_get_contents('php://input');
+        $this->body = json_decode($bodyString, true);
+        parse_str($_SERVER['QUERY_STRING'], $this->params);
+
+
+        switch ($httpMethod) {
+            case "GET":
+                // call get
+                $this->get();
+                break;
+            case "POST":
+                // call post
+                $this->post();
+                break;
+            case "PATCH":
+                // call patch
+                $this->patch();
+                break;
+            case "DELETE":
+                // call delete
+                $this->delete();
+                break;
+            case "OPTIONS":
+                // call options
+                $this->options();
+                break;
+            default:
+                $this->unsupportedHttpMethod();
+                break;
+        }
+    }
+
+    protected function unsupportedHttpMethod() {
+        http_response_code(400);
+        echo 'Unsupported HTTP method';
+    }
+
+    protected function notFound() {
+        http_response_code(404);
+        echo 'entity not found';
+    }
+
+    protected function duplicatedId() {
+        echo 'duplicated id';
+        http_response_code(409);
+    }
+
+    protected function ok($result) {
+        $this->responseRequest(200, $result);
+    }
+
+    protected function created($result) {
+        $this->responseRequest(201, $result);
+    }
+
+    protected function responseRequest($code, $result) {
+        echo json_encode($result);
+        http_response_code($code);
+    }
+    
+    protected function getQueryParameter($key, $required = false) {
+        $value = null;
+        if($this->params != null) {
+            $value = $this->params[$key];
+        }
+        if($required && $value == null) {
+            http_response_code(400);
+            echo $key . ' not set';
+        }
+
+        return $value;
+    }
+}
+?>

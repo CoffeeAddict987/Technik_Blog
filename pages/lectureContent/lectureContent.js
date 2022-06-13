2d6d375e7c2b14bf57118d8e106826e486a73544
Phil012 <phillip.rederath@t-online.de>
2022-06-13 18:01:41 +0200

    changes

--- pages/lectureContent/lectureContent.js
+++ pages/lectureContent/lectureContent.js
@@ -2,4 +2,25 @@ class LectureContent extends Page {
     constructor() {
         super('lectureContent');
     }
+
+
+    render(parentSelector) {
+        const contents = getDatabaseContent();
+        const contentElements = [];
+        console.log('render', contents);
+        const wrapper = $('<div />', { class: 'database-content-wrapper'  });
+        contents.forEach(content => {
+            const contentElement = $('<div />', { class:  'database-content'  });
+            contentElement.load('./pages/databaseContent/databaseContent.html', () => {
+                contentElement.find('.title2').text(content.title),
+                contentElement.find('.text2').text(content.text);
+            });
+            contentElements.push(contentElement);
+        });
+        wrapper.append(contentElements);
+        $(parentSelector).empty().append(wrapper);
+        
+    }
+    
 }
+

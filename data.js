3b0cca4d22309f01a9942bc64083313d2b5d5c29
Markus Surma <s192083@student.dhbw-mannheim.de>
2022-06-24 14:51:08 +0200

    Final_V1

--- data.js
+++ data.js
@@ -1,7 +1,23 @@
-const baseUrl = 'http://localhost/TechnikBlog/databaseInterests.php';
+const baseUrlArticles = 'http://localhost/TechnikBlog/databaseArticles.php';
+const baseUrlInterests = 'http://localhost/TechnikBlog/databaseInterests.php';
 
-async function getDatabaseContent() {
-    const response = await fetch(baseUrl);
+
+async function getArticlesContentById(id) {
+    // const response = await fetch(`${baseUrlArticles}?id=${id}`);
+    const response = await fetch('' + baseUrlArticles + '?id=' + id + '&quantity&tags'); // equivalent
+    const result = await response.json();
+    return result;
+}
+
+async function getArticlesContentByTags(tag) {
+    // const response = await fetch(`${baseUrlArticles}?id=${id}`);
+    const response = await fetch('' + baseUrlArticles + '?id&quantity=2&tags=' + tag); // equivalent
+    const result = await response.json();
+    return result;
+}
+
+async function getInterestsContent() {
+    const response = await fetch(baseUrlInterests);
     const result = await response.json();
     return result;
 }

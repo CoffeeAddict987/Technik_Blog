2e434ce09d25d451488744778ec18255a70ba7c3
Markus Surma <s192083@student.dhbw-mannheim.de>
2022-06-24 12:25:34 +0200

    add backendconnectionV3

--- pages/Blog/blog.js
+++ pages/Blog/blog.js
@@ -4,11 +4,26 @@ class Blog extends Page {
     }
 
 
-    render(parentSelector) {
-        const contents = getDatabaseContent();
-        console.log(contents);
-        
+    async render(parentSelector) {
+        const contents = await getDatabaseContent();
+        const contentElements = [];
+        console.log('render', contents);
+        const wrapper = $('<div />', { class: 'database-content-wrapper' });
+        contents.forEach((content, index) => {
+            const contentElement = $('<div />', { class: 'database-content' });
+            contentElement.load('./pages/databaseContent/databaseContent.html', () => {
+                contentElement.find('.title2').text(content.id);
+                contentElement.find('.text2').text(content.name);
+                if (index >= contents.length - 1) {
+                    $(parentSelector).empty().append(wrapper);
+                }
+            });
+            contentElements.push(contentElement);
+        });
+        wrapper.append(contentElements);
+        // $(parentSelector).empty().append(wrapper);
     }
+
     
 }
 

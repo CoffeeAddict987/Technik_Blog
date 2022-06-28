c5fd180d33331b3d4466c3fef97f5456e02190a1
Markus Surma <s192083@student.dhbw-mannheim.de>
2022-06-28 21:13:24 +0200

    add dynamic pictures V1

--- pages/druckerContent/druckerContent.js
+++ pages/druckerContent/druckerContent.js
@@ -3,25 +3,7 @@ class DruckerContent extends Page {
         super('druckerContent');
     }
     
-    /*async render(parentSelector) {
-
-        const tags = 2;             //Tag 2 entspricht dem Tag Drucker
-        const contents = await getArticlesContentByTags(tags);
-        const contentElements = [];
-        console.log('render', contents);
-        const wrapper = $('<div />', { class: 'database-content-wrapper' });
-
-        const contentElement = $('<div />', { class: 'database-content' });
-            contentElement.load('./pages/databaseContent/databaseContent.html', () => {
-                contentElement.find('.drucker-title').text(contents.title);
-                contentElement.find('.drucker-content').text(contents.content);
-                $(parentSelector).empty().append(wrapper);
-            });
-            contentElements.push(contentElement);
-            wrapper.append(contentElements);*/
-
         async render(parentSelector) {
-        
         const tags = 2;             //Tag 2 entspricht dem Tag Drucker
         const contents = await getArticlesContentByTags(tags);
         const contentElements = [];
@@ -31,6 +13,7 @@ class DruckerContent extends Page {
             const contentElement = $('<div />', { class: 'database-content' });
             contentElement.load('./pages/druckerContent/druckerContent.html', () => {
                 contentElement.find('.drucker-title').text(content.title);
+                contentElement.find('.test').append($(`<img class="picture" src="` + content.path + `">`));
                 contentElement.find('.drucker-content').text(content.content);
                 if (index >= contents.length - 1) {
                     $(parentSelector).empty().append(wrapper);
@@ -42,5 +25,7 @@ class DruckerContent extends Page {
         // $(parentSelector).empty().append(wrapper);
     }
     
+    
+
     }
 
